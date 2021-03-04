c786300cf76167f448f196385d5668c06865077a
leadengrobot <61973743+leadengrobot@users.noreply.github.com>
2021-03-04 11:25:18 +0200

    code for lecture 2

---
+++ lecture_2_03_Mar.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Enter Your Name
+            Console.WriteLine("Enter name:");
+            // Read your name into a string variable
+            string name = Console.ReadLine();
+
+            // Enter Your Surname
+            Console.WriteLine("Enter surname:");
+            // Read your name into a string variable
+            string surname = Console.ReadLine();
+
+            // Print your name and surname
+            Console.WriteLine("Your name is: " + name + " Your surname is: " + surname);
+
+            // Enter your age
+            Console.WriteLine("Age:");
+
+            //Convert the age from a String to Int
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your age is: " + age);
+            
+        }
+    }
+}
